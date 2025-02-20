3b4f87e472a02eee2ed9e35289826b891c01afff
Apollo-programmer <nguyentai10122006@gmail.com>
2025-02-20 08:02:35 +0700

    inputBox

---
+++ inc/GUI/inputBox.h
@@ -0,0 +1,26 @@
+// InputBox.h
+#ifndef INPUTBOX_H
+#define INPUTBOX_H
+
+#include "raylib.h"
+#include <string>
+
+class InputBox {
+private:
+    Rectangle box;
+    std::string text;
+    int maxLength;
+    bool isActive;
+    Color boxColor;
+    Color textColor;
+    float blinkTimer;
+    bool showCursor;
+
+public:
+    InputBox(float x, float y, float width, float height, int maxLen = 20, Color boxCol = LIGHTGRAY, Color txtCol = BLACK);
+    void Update();
+    void Draw();
+    std::string GetText() const;
+};
+
+#endif // INPUTBOX_H
---
+++ src/inputBox.cpp
@@ -0,0 +1,49 @@
+// InputBox.cpp
+#include "InputBox.h"
+
+InputBox::InputBox(float x, float y, float width, float height, int maxLen, Color boxCol, Color txtCol)
+    : box{x, y, width, height}, maxLength(maxLen), isActive(false), boxColor(boxCol), textColor(txtCol), blinkTimer(0), showCursor(true) {}
+
+void InputBox::Update() {
+    if (CheckCollisionPointRec(GetMousePosition(), box) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+        isActive = true;
+    } else if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+        isActive = false;
+    }
+
+    if (isActive) {
+        int key = GetCharPressed();
+        while (key > 0) {
+            if ((key >= 32 && key <= 126) && text.length() < maxLength) {
+                text += static_cast<char>(key);
+            }
+            key = GetCharPressed();
+        }
+
+        if (IsKeyPressed(KEY_BACKSPACE) && !text.empty()) {
+            text.pop_back();
+        }
+    }
+
+    blinkTimer += GetFrameTime();
+    if (blinkTimer >= 0.5f) {
+        blinkTimer = 0;
+        showCursor = !showCursor;
+    }
+}
+
+void InputBox::Draw() {
+    DrawRectangleRec(box, isActive ? DARKGRAY : boxColor);
+    DrawRectangleLinesEx(box, 2, BLACK);
+    DrawText(text.c_str(), box.x + 5, box.y + box.height / 4, 20, textColor);
+    
+    if (isActive && showCursor) {
+        DrawText("|", box.x + 5 + MeasureText(text.c_str(), 20), box.y + box.height / 4, 20, textColor);
+    }
+}
+
+std::string InputBox::GetText() const {
+    return text;
+}
+
+
